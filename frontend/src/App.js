973f015cd6361eea02a5acd2cf1cb36557ef9fca
vnkbrand <vnkbrand@gmail.com>
2020-09-30 16:53:14 +0200

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // The return must only have a Single element - nothing more (one div etc.)
+    <>
+      <h1>Welcome To BitVice</h1>
+    </>
+  )
+}
+
+export default App
